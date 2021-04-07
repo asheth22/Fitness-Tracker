7ad4ede47543a88a7ba458be36cd0b215b5b6c81
Anjali Sheth <anjali.sheth@otis.com>
2021-04-07 10:42:41 -0400

    added deployment env variable

--- server.js
+++ server.js
@@ -14,7 +14,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhostJHUBootcamp',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.use(require("./routes/api.js"));
 app.use(require("./routes/html.js"));
